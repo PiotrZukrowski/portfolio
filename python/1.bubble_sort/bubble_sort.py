7f2cf6ddad925f24fd0c4e7fce9b016fa57ed5ae
Piotr Żukrowski <111241777+PiotrZukrowski@users.noreply.github.com>
2024-01-07 18:47:41 +0100

    Update bubble_sort.py
    Add variable data types

--- python/1.bubble_sort/bubble_sort.py
+++ python/1.bubble_sort/bubble_sort.py
@@ -1,12 +1,12 @@
 
 
-def bubble_sort(iterable, reverted=False):
+def bubble_sort(iterable, reverted=False: bool):
 
     """
     Sort iterable in-place (i.e. the list itself is modified), stable (i.e. the
         order of two equal elements is maintained) and return sorted.
 
-    :param iterable: Sort the list in ascending order and return
+    :param iterable: Data
     :param reverted: Indicate descending order
     :return: Sorted iterable
     """
@@ -20,20 +20,20 @@ def bubble_sort(iterable, reverted=False):
     if reverted and not hasattr(iterable, '__lt__'):
         raise TypeError(f"'{type(iterable).__name__}' object does not define '__lt__' operator")
 
-    length = len(iterable)
+    length: int = len(iterable)
     for i in range(length - 1):
-        is_ongoing = False
+        is_ongoing: bool = False
         for j in range(length - 1 - i):
             if iterable[j] > iterable[j+1] if not reverted else iterable[j] < iterable[j+1]:
                 iterable[j], iterable[j+1] = iterable[j+1], iterable[j]
-                is_ongoing = True
+                is_ongoing: bool = True
         if not is_ongoing:
             break
     return iterable
 
 
 if __name__ == '__main__':
-    a = [0, 2, 1, 3]
+    a: list = [0, 2, 1, 3]
     print(f'{a}')
     print(f'{bubble_sort(a) = }')
     print(f'{bubble_sort(a, reverted=True) = }')
