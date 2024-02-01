b8519cdf5cc53501b86d50510bded1e4d8b901df
Piotr Żukrowski <111241777+PiotrZukrowski@users.noreply.github.com>
2024-02-01 18:23:01 +0100

    Create main.cpp

---
+++ c++/association_algorithm/main.cpp
@@ -0,0 +1,14 @@
+/* Wersja 0 */
+
+#include <iostream>
+#include "GRAF_SKOJARZENIE.h"
+
+#include <cstdlib>  // srand();
+#include <ctime>    // srand( time(NULL) );
+
+int main()
+{
+    srand(time(NULL));
+    Lista1112_Skojarzenie_w0();
+    return 0;
+}
