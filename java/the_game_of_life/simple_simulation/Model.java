23e8deb285f7ac6f22e53bb579ab1f5d6156aec2
Piotr Żukrowski <111241777+PiotrZukrowski@users.noreply.github.com>
2024-04-15 11:35:37 +0200

    Update Model.java - Delete developer notes

--- java/the_game_of_life/simple_simulation/Model.java
+++ java/the_game_of_life/simple_simulation/Model.java
@@ -14,17 +14,6 @@ public class Model {
     * @return A table containing pairs of cell coordinates.
 */
 
-/*
-    if type(max_row) is not int:
-        raise TypeError(f"'max_row' parameter must be an int, not {type(max_row)}")
-    if type(max_col) is not int:
-        raise TypeError(f"'max_row' parameter must be an int, not {type(max_col)}")
-    if max_row < 1:
-        raise ValueError(f"'max_row' parameter must be greater than zero")
-    if max_col < 1:
-        raise ValueError(f"'max_col' parameter must be greater than zero")
-*/
-
     boolean[][] matrix = new boolean[maxRow][maxCol];
     return matrix;
   }
@@ -36,14 +25,6 @@ public class Model {
 
     * @param matrix The table containing pairs of cell coordinates.
     * @param return Null.
-*/
-
-/*
-    if type(matrix) is not list:
-        raise TypeError(f"'matrix' parameter must be a list, not {type(matrix)}")
-    for i in range(len(matrix)):
-        if type(matrix[i]) is not list:
-            raise TypeError(f"'matrix[{i}]' parameter must be a list, not {type(matrix[i])}")
 */
 
     Random random = new Random();
@@ -61,17 +42,6 @@ public class Model {
 
     * @param matrix The table containing old pairs of cell coordinates.
     * @param return A table containing new pairs of cell coordinates.
-*/
-
-/*
-    if type(matrix) is not list:
-        raise TypeError(f"'matrix' parameter must be a list, not {type(matrix)}")
-    for i in range(len(matrix)):
-        if type(matrix[i]) is not list:
-            raise TypeError(f"'matrix[{i}]' parameter must be a list, not {type(matrix[i])}")
-        for j in range(len(matrix[i])):
-            if type(matrix[i][j]) is not bool:
-                raise TypeError(f"'matrix[{i}][{j}]' parameter must be a bool, not {type(matrix[i])}")
 */
 
     boolean[][] matrixNew = new boolean[matrix.length][];
@@ -100,33 +70,6 @@ public class Model {
     * @param return None.
 */
 
-/*
-    if type(matrix_old) is not list:
-        raise TypeError(f"'matrix_old' parameter must be a list, not {type(matrix_old)}")
-    if type(matrix_new) is not list:
-        raise TypeError(f"'matrix_new' parameter must be a list, not {type(matrix_new)}")
-    if type(pos_row) is not int:
-        raise TypeError(f"'pos_row' parameter must be an int, not {type(pos_row)}")
-    if type(pos_col) is not int:
-        raise TypeError(f"'pos_col' parameter must be an int, not {type(pos_col)}")
-    if pos_row < 0:
-        raise ValueError(f"'pos_row' parameter must be a non-negative integer")
-    if pos_col < 0:
-        raise ValueError(f"'pos_col' parameter must be a non-negative integer")
-    if pos_row > len(matrix_new) + 1:
-        raise ValueError(f"'pos_row' parameter must be lower than list Y-axis")
-    if pos_col > len(matrix_new[pos_row]) + 1:
-        raise ValueError(f"'pos_col' parameter must be lower than list X-axis")
-    if type(matrix_old[pos_row]) is not list:
-        raise TypeError(f"'matrix_old[{pos_row}]' parameter must be a list, not {type(matrix_old[pos_row])}")
-    if type(matrix_old[pos_row][pos_col]) is not bool:
-        raise TypeError(f"'matrix_old[{pos_row}][{pos_col}]' parameter must be a bool, not {type(matrix_old[pos_col])}")
-    if type(matrix_new[pos_row]) is not list:
-        raise TypeError(f"'matrix_new[{pos_row}]' parameter must be a list, not {type(matrix_new[pos_row])}")
-    if type(matrix_new[pos_row][pos_col]) is not bool:
-        raise TypeError(f"'matrix_new[{pos_row}][{pos_col}]' parameter must be a bool, not {type(matrix_new[pos_col])}")
-*/
-
     boolean oldValue = matrixOld[posRow][posCol];
     int counter = getNeighboursCounter(matrixOld, posRow, posCol);
 
@@ -152,27 +95,6 @@ public class Model {
     * @param posRow Index of the row.
     * @param posCol Index of the column.
     * @param return A number of neighbours.
-*/
-
-/*
-    if type(matrix) is not list:
-        raise TypeError(f"'matrix' parameter must be a list, not {type(matrix)}")
-    if type(pos_row) is not int:
-        raise TypeError(f"'pos_row' parameter must be an int, not {type(pos_row)}")
-    if type(pos_col) is not int:
-        raise TypeError(f"'pos_col' parameter must be an int, not {type(pos_col)}")
-    if pos_row < 0:
-        raise ValueError(f"'pos_row' parameter must be a non-negative integer")
-    if pos_col < 0:
-        raise ValueError(f"'pos_col' parameter must be a non-negative integer")
-    if pos_row > len(matrix) + 1:
-        raise ValueError(f"'pos_row' parameter must be lower than list Y-axis")
-    if pos_col > len(matrix[0]) + 1:
-        raise ValueError(f"'pos_col' parameter must be lower than list X-axis")
-    if type(matrix[pos_row]) is not list:
-        raise TypeError(f"'matrix[{pos_row}]' parameter must be a list, not {type(matrix[pos_row])}")
-    if type(matrix[pos_row][pos_col]) is not bool:
-        raise TypeError(f"'matrix[{pos_row}][{pos_col}]' parameter must be a bool, not {type(matrix[pos_col])}")
 */
 
     int rowLen = matrix.length;
