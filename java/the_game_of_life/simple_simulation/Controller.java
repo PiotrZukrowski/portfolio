49c3113a97cde3842cb77b9f5b8109fa610f9650
Piotr Żukrowski <111241777+PiotrZukrowski@users.noreply.github.com>
2024-04-15 11:34:27 +0200

    Update Controller.java - Delete developer notes

--- java/the_game_of_life/simple_simulation/Controller.java
+++ java/the_game_of_life/simple_simulation/Controller.java
@@ -16,13 +16,6 @@ public class Controller {
     @param return None.
 */
 
-/*
-    if type(matrix) is not list:
-        raise TypeError(f"'matrix' parameter must be a list, not {type(matrix)}")
-    if type(widgets) is not dict:
-        raise TypeError(f"'widgets' parameter must be a dict, not {type(widgets)}")
-*/
-
     int cellSize = (int) widgets.get("cellSize");
     ((JComponent) widgets.get("screen")).repaint(new Rectangle(0, 0,
                                                                cellSize * matrix[0].length,
@@ -38,13 +31,6 @@ public class Controller {
     @param matrix The table containing old pairs of cell coordinates.
     @param widgets The map object containing JComponents and cellSize.
     @param return A table containing new pairs of cell coordinates.
-*/
-
-/*
-    if type(matrix) is not list:
-        raise TypeError(f"'matrix' parameter must be a list, not {type(matrix)}")
-    if type(widgets) is not dict:
-        raise TypeError(f"'widgets' parameter must be a dict, not {type(widgets)}")
 */
 
     boolean[][] matrixNew = Model.getNextGeneration(matrix);
@@ -65,23 +51,6 @@ public class Controller {
     @param return None.
 */
 
-/*
-    if type(widgets) is not dict:
-        raise TypeError(f"'widgets' parameter must be a dict, not {type(widgets)}")
-    if type(pos_row) is not int:
-        raise TypeError(f"'pos_row' parameter must be an int, not {type(pos_row)}")
-    if type(pos_col) is not int:
-        raise TypeError(f"'pos_col' parameter must be an int, not {type(pos_col)}")
-    if pos_row < 0:
-        raise ValueError(f"'pos_row' parameter must be a non-negative integer")
-    if pos_col < 0:
-        raise ValueError(f"'pos_col' parameter must be a non-negative integer")
-    if pos_row > widgets['screen'].winfo_reqheight():
-        raise ValueError(f"'pos_row' parameter must be lower than list Y-axis")
-    if pos_col > widgets['screen'].winfo_reqwidth():
-        raise ValueError(f"'pos_col' parameter must be lower than list X-axis")
-*/
-
     int cellSize = (int) widgets.get("cellSize");
     ((JComponent) widgets.get("screen")).repaint(new Rectangle(posCol * cellSize,
                                                                posRow * cellSize,
@@ -99,17 +68,6 @@ public class Controller {
     @param widgets The map object containing JComponents and cellSize.
     @param cycles The number of generations to simulate.
     @param return A table containing new pairs of cell coordinates.
-*/
-
-/*
-    if type(matrix) is not list:
-        raise TypeError(f"'matrix' parameter must be a list, not {type(matrix)}")
-    if type(widgets) is not dict:
-        raise TypeError(f"'widgets' parameter must be a dict, not {type(widgets)}")
-    if type(cycles) is not int:
-        raise TypeError(f"'cycles' parameter must be an int, not {type(cycles)}")
-    if cycles < 1:
-        raise ValueError(f"'cycles' parameter must be greater than zero")
 */
 
     for (int i = 0; i < cycles; i++) {
